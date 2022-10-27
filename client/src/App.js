7f1e6d43e326613dbb2e7af9f4b011d0dc303775
Mar550 <marouane.merno@gmail.com>
2022-10-27 11:02:29 +0100

    Fixing errors after reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/Login';
 import Register from './pages/Register';
-import Expense from './pages/Expense';
-import Income from './pages/Income';
+import Expense from './pages/expense/Expense';
+import Income from './pages/income/Income';
 import Dashboard from './pages/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import './App.css';
+import Popup from "./pages/expense/Popup";
+import Graph from "./pages/chart/Chart";
+import New from "./pages/chart/New";
+import Home from "./pages/Home";
+
 
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dahsboard" element={<Dashboard />} />
             <Route path="/expense" element={<Expense />} />
             <Route path="/income" element={<Income />} />
+            <Route path="/popup" element={<Popup />} />
+            <Route path="/chart/:id" element={<Graph />} />
+            <Route path="/chart/create" element={<New />} />
         </Routes>
       </BrowserRouter>
     </div>

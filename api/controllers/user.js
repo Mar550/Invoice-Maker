fa1d16639aaebcf455fe94f41153b49c40c4b6f5
Marouane <m.marouane14@gmail.com>
2023-03-19 01:06:52 +0000

    Fixed: Case sensitive error controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 router.get('/usertest', (req,res) => {
     res.send("user test is successful");
